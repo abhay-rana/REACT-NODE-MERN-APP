a66e047c04e70e1d91b09cb474d04ae477da1833
Abhay Rana <abhay.rana@healthmug.com>
2022-08-14 22:25:15 +0530

    server changes

---
+++ backend/routes/user-routes.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+// import { createUser, deleteUserById, getAllUsers, getUserById, updateUserById } from "../controller/userController.js";
+
+const router = express.Router();
+
+// router.route("/user").get(getAllUsers);
+// router.route("/user/new").post(createUser);
+// router.route("/user/:id").get(getUserById);
+// router.route("/user/update/:id").put(updateUserById);
+// router.route("/user/delete/:id").delete(deleteUserById);
+
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,9 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import "dotenv/config";
 
-import router from "./routes/routesController.js";
 import connectDb from "./config/db.js";
+import todo_routes from "./routes/todo-routes.js";
+import user_routes from "./routes/user-routes.js";
 
 const app = express();
 
@@ -12,9 +13,25 @@ connectDb(); //for connection with the database
 
 //middelwares
 app.use(cors());
-app.use("/", router);
 app.use(bodyParser.json());
 
+//mounting of routes
+app.use("/", todo_routes);
+app.use("/", user_routes);
+
+//server is listening
+app.get("", (req, res) => {
+	res.send("Server is Listening successfully");
+});
+
+//error handling of unhandeled routes
+app.use("*", (req, res) => {
+	res.status(404).json({
+		status: "fail",
+		message: `this ${req.originalUrl} route is not present`,
+	});
+});
+
 app.listen(process.env.PORT, () => {
-	console.log("server is starts at ", process.env.PORT);
+	console.log("server is running at port", process.env.PORT);
 });
