03d6bfaed69e1b1bb3fce1cb8b331e4ed8ecc57a
Abhay Rana <abhay.rana@healthmug.com>
2022-09-13 22:55:19 +0530

    use of await

--- backend/middleware/authentication-middleware.js
+++ backend/middleware/authentication-middleware.js
@@ -3,7 +3,7 @@ import "dotenv/config";
 
 //middleware for the authentication
 
-const isAuthenticatedUser = (req, res, next) => {
+const isAuthenticatedUser = async (req, res, next) => {
 	const { jwt_token } = req.cookies;
 	if (!jwt_token) {
 		return res.json({
@@ -12,8 +12,8 @@ const isAuthenticatedUser = (req, res, next) => {
 		});
 	}
 	const decode = jwt.verify(jwt_token, process.env.JWT_SECRET);
-	req.user=await User.findById(decode._id);
-    next(); 
+	req.user = await User.findById(decode._id);
+	next();
 };
 
 export default isAuthenticatedUser;
